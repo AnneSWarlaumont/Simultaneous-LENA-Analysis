617c74f7004e2d848aaea21eff851589f5eae467
AnneSWarlaumont <anne.warlaumont@gmail.com>
2016-06-22 15:00:27 -0700

    Updated relabel.py, so updated postsyncscript.py accordingly

--- postsyncscript.py
+++ postsyncscript.py
@@ -10,4 +10,4 @@ postPluralEyes.getSubRecInfo('/Users/awarlau/LENAInteraction/ECECStudy/2015-05-2
 
 subprocess.call(["perl","/Users/awarlau/HomeBankCode/lena-its-tools/segments.pl",'/Users/awarlau/LENAInteraction/ECECStudy/ITSFiles/e20150601_171803_010569.its','/Users/awarlau/LENAInteraction/ECECStudy/LENASegments/e20150601_171803_010569_LENASegments.csv'])
 
-relabel.relabel_human_near('/Users/awarlau/LENAInteraction/ECECStudy/LENASegments/e20150601_171803_010569_LENASegments.csv','/Users/awarlau/LENAInteraction/ECECStudy/WAVFiles/e20150601_171803_010569.wav','/Users/awarlau/LENAInteraction/ECECStudy/CorrectedSegments/e20150601_171803_010569_CorrectedSegments.csv',17.2666666667,3373.18) # In future, we will want to get these inputs automatically from reading the day and room's reltimes.csv file.
+relabel.relabel_by_timebin('/Users/awarlau/LENAInteraction/ECECStudy/WAVFiles/e20150601_171803_010569.wav','/Users/awarlau/LENAInteraction/ECECStudy/CorrectedSegments/e20150601_171803_010569_HumanTimebinLabels.csv',17.2666666667,3373.18,1) # In future, we will want to get these inputs automatically from reading the day and room's reltimes.csv file.
